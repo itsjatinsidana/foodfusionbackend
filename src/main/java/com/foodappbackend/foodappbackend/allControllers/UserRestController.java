e4df9bf3b94675a50290a16491e6207127a6f1ee
JATIN SIDANA <108786329+itsjatinsidana@users.noreply.github.com>
2024-08-16 13:22:56 +0530

    new changes

--- src/main/java/com/foodappbackend/foodappbackend/allControllers/UserRestController.java
+++ src/main/java/com/foodappbackend/foodappbackend/allControllers/UserRestController.java
@@ -425,7 +425,8 @@ public class UserRestController {
     public String userOrders(@RequestParam String email) {
         System.out.println("email issj........" + email);
         String ans = new RDBMS_TO_JSON().generateJSON("SELECT * FROM orders "
-                + "INNER JOIN cart ON orders.cart_id = cart.cart_id && cart.useremail='" + email + "'"
+                + "INNER JOIN cart ON orders.cart_id = cart.cart_id "
+                + " cart.useremail='" + email + "'"
                 + " ORDER BY orders.created_at DESC ");
 
         return ans;
