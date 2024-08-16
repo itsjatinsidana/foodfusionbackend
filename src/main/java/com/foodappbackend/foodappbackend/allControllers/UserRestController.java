2a057bc4b13dbb18f9bbc0a0f9a7a633e8f3abaf
JATIN SIDANA <108786329+itsjatinsidana@users.noreply.github.com>
2024-08-16 10:45:02 +0530

    update in orders table

--- src/main/java/com/foodappbackend/foodappbackend/allControllers/UserRestController.java
+++ src/main/java/com/foodappbackend/foodappbackend/allControllers/UserRestController.java
@@ -28,114 +28,114 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class UserRestController {
-    
+
     @PostMapping("/usersignup")
     public String userSignup(@RequestParam String useremail,
             @RequestParam String username,
             @RequestParam String userpassword,
             @RequestParam String usernumber
     ) {
-        
+
         try {
             //EXCEPTION ERROR MAKE executeSQL
             ResultSet rs = DBLoader.executeSQL("SELECT * FROM user_signup where user_email ='" + useremail + "' ");
-            
+
             if (rs.next()) {
                 return "fail";
             } else {
-                
+
                 rs.moveToInsertRow();
                 //insert workbench coloum name
                 rs.updateString("user_email", useremail);
                 rs.updateString("user_name", username);
                 rs.updateString("user_password", userpassword);
                 rs.updateString("user_number", usernumber);
-                
+
                 rs.updateString("user_active", "no");
                 rs.insertRow();
-                
+
                 return "success";
             }
-            
+
         } catch (Exception ex) {
             return ex.toString();
-            
+
         }
     }
-    
+
     @PostMapping("/checkuserlogin")
-    
+
     public String CheckLogin(@RequestParam String useremail, @RequestParam String userpassword, HttpSession session) {
         try {
 
             //EXCEPTION ERROR MAKE executeSQLs
             ResultSet rs = DBLoader.executeSQL("select * from user_signup where user_email='" + useremail + "' and user_password='" + userpassword + "'");
-            
+
             if (rs.next()) {
                 String user_name = rs.getString("user_name");
                 session.setAttribute("user_name", user_name);
-                
+
                 System.out.println(user_name);
                 JSONObject response = new JSONObject();
                 response.put("status", "success");
                 response.put("user_name", user_name);
-                
+
                 return response.toString();
             } else {
                 return "{\"status\":\"fail\"}";
             }
-            
+
         } catch (Exception ex) {
             ex.printStackTrace();
             return "{\"status\":\"error\", \"message\":\"" + ex.toString() + "\"}";
         }
-        
+
     }
-    
+
     @RequestMapping(value = "/getuserdetails", produces = "application/json", method = RequestMethod.GET)
     public String getCities() {
         String ans = new RDBMS_TO_JSON().generateJSON("SELECT * FROM user_signup");
         return ans;
     }
-    
+
     @PostMapping("/updateuserstatus")
     public String editMenuItem(@RequestParam String useremail, HttpSession session) {
-        
+
         System.out.println(useremail);
         String ans = "";
-        
+
         try {
             //EXCEPTION ERROR MAKE executeSQL
             ResultSet rs = DBLoader.executeSQL("SELECT * FROM user_signup where user_email ='" + useremail + "' ");
             if (rs.next()) {
-                
+
                 rs.updateString("user_active", "yes");
-                
+
                 rs.updateRow();
-                
+
                 return "success";
             } else {
-                
+
                 return "fail";
             }
-            
+
         } catch (Exception ex) {
             return ex.toString();
-            
+
         }
     }
-    
+
     @PostMapping("/showusermenuitems")
     public String showUserMenuItems() {
         String query = "SELECT mi.*, mc.category_name "
                 + "FROM menu_items mi "
                 + "JOIN menu_category mc ON mi.category_id = mc.category_id";
-        
+
         String ans = new RDBMS_TO_JSON().generateJSON(query);
-        
+
         return ans;
     }
-    
+
     @PostMapping("/addtocart")
     public String addToCart(@RequestParam String useremail,
             @RequestParam String name,
@@ -143,7 +143,7 @@ public class UserRestController {
             @RequestParam String price,
             @RequestParam String category_name,
             @RequestParam String photo) {
-        
+
         System.out.println(useremail);
         System.out.println(items_id);
         System.out.println(name);
@@ -151,7 +151,7 @@ public class UserRestController {
         try {
             //EXCEPTION ERROR MAKE executeSQL
             ResultSet rs = DBLoader.executeSQL("SELECT * FROM cart");
-            
+
             rs.moveToInsertRow();
             //insert workbench coloum name
             rs.updateString("useremail", useremail);
@@ -163,16 +163,16 @@ public class UserRestController {
             rs.updateString("price", price);
             rs.updateString("total_amount", price);
             rs.insertRow();
-            
+
             return "success";
-            
+
         } catch (Exception ex) {
             return ex.toString();
-            
+
         }
-        
+
     }
-    
+
     @PostMapping("/showcartitems")
     public String showCartItems(@RequestParam String useremail) {
         System.out.println(useremail);
@@ -181,31 +181,31 @@ public class UserRestController {
         System.out.println("items are " + ans);
         return ans;
     }
-    
+
     @PostMapping("/deletecartitem")
     public String deleteCartItem(@RequestParam String cart_id) {
         try {
-            
+
             System.out.println(cart_id);
             //EXCEPTION ERROR MAKE executeSQLs
             ResultSet rs = DBLoader.executeSQL("SELECT * FROM cart where cart_id ='" + cart_id + "' ");
-            
+
             if (rs.next()) {
-                
+
                 rs.deleteRow();
                 System.out.println("deleted succusfully");
                 return "success";
             } else {
                 return "fail";
             }
-            
+
         } catch (Exception ex) {
             ex.printStackTrace();
             return ex.toString();
         }
-        
+
     }
-    
+
     @PostMapping("/showuseraddress")
     public String showUserAddresss(@RequestParam String useremail) {
         System.out.println(useremail);
@@ -213,7 +213,7 @@ public class UserRestController {
         System.out.println("address is " + ans);
         return ans;
     }
-    
+
     @PostMapping("/showuseraddressdata")
     public String showUserAddresssData(@RequestParam String userid) {
         System.out.println("user id is" + userid);
@@ -222,7 +222,7 @@ public class UserRestController {
         System.out.println("address is " + ans);
         return ans;
     }
-    
+
     @PostMapping("/addaddress")
     public String addAdress(@RequestParam int userid,
             @RequestParam String flataddress,
@@ -235,12 +235,11 @@ public class UserRestController {
             @RequestParam String longitude) {
 
         try {
-           
+
             ResultSet rs = DBLoader.executeSQL("SELECT * FROM addresses ");
-         
 
             rs.moveToInsertRow();
-            
+
             rs.updateInt("user_signup_id", userid);
             rs.updateString("flat_no", flataddress);
             rs.updateString("street",
@@ -251,55 +250,53 @@ public class UserRestController {
             rs.updateString("map_address", mapaddress);
             rs.updateString("latitude", latitude);
             rs.updateString("longitude", longitude);
-            
+
             rs.insertRow();
-            
+
             return "success";
-            
-        } 
-        catch (SQLException e) {
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return "fail";
+        } catch (Exception e) {
             e.printStackTrace();
             return "fail";
         }
-        catch (Exception e) {
-        e.printStackTrace();
-        return "fail";
     }
-    }
-    
+
     @PostMapping("/updatecartquantity")
     public String updateCartQuantity(@RequestParam String cart_id, @RequestParam int quantity, @RequestParam int totalAmount) {
-        
+
         String ans = "";
         try {
             //EXCEPTION ERROR MAKE executeSQL
             ResultSet rs = DBLoader.executeSQL("SELECT * FROM cart where cart_id ='" + cart_id + "' ");
             if (rs.next()) {
-                
+
                 rs.updateInt("quantity", quantity);
                 rs.updateInt("total_amount", totalAmount);
-                
+
                 rs.updateRow();
-                
+
                 return "success";
             } else {
-                
+
                 return "fail";
             }
-            
+
         } catch (Exception ex) {
             return ex.toString();
         }
-        
+
     }
-    
+
     @GetMapping("/fetchOrderDetails")
     public String showOrder(@RequestParam String itemIds) {
-        
+
         String ans = new RDBMS_TO_JSON().generateJSON("select * from cart where cart_id in (" + itemIds + ")");
         System.out.println("hdgjdbrfjkbjk " + ans);
         return ans;
-        
+
     }
 
 //    @PostMapping("/success")
@@ -359,7 +356,7 @@ public class UserRestController {
             @RequestParam String address,
             @RequestParam String amount,
             @RequestParam String cart_id) {
-        
+
         System.out.println("Payment successful:");
         System.out.println("razpaymentid: " + razorpaymentid);
         System.out.println("razorderid: " + razororderid);
@@ -367,7 +364,7 @@ public class UserRestController {
         System.out.println("amount: " + amount);
         System.out.println("cart id is: " + cart_id);
         System.out.println("address: " + address);
-        
+
         try {
             // Clean and parse amount
             String cleanedAmount = amount.replaceAll("[^0-9]", "");
@@ -376,7 +373,7 @@ public class UserRestController {
             // Split cart_id string by commas and process each ID
             String[] cartIds = cart_id.split(",");
             boolean allCartIdsValid = true;
-            
+
             for (String idStr : cartIds) {
                 int cartIdInt = Integer.parseInt(idStr.trim());
 
@@ -390,7 +387,7 @@ public class UserRestController {
 
                 // Proceed with inserting into orders table
                 ResultSet rsOrders = DBLoader.executeSQL("SELECT * FROM orders WHERE cart_id = " + cartIdInt);
-                
+
                 if (rsOrders.next()) {
                     System.out.println("Order with cart_id " + cartIdInt + " already exists.");
                     continue; // Skip this ID if the order already exists
@@ -404,17 +401,17 @@ public class UserRestController {
                     rsOrders.updateString("address", address);
                     rsOrders.updateString("status", "pending");
                     rsOrders.insertRow();
-                    
+
                     System.out.println("Inserted order for cart_id " + cartIdInt);
                 }
             }
-            
+
             if (allCartIdsValid) {
                 return "success";
             } else {
                 return "Some cart IDs were invalid or already had orders.";
             }
-            
+
         } catch (NumberFormatException e) {
             return "Invalid number format: " + e.getMessage();
         } catch (SQLException e) {
@@ -423,15 +420,15 @@ public class UserRestController {
             return ex.toString();
         }
     }
-    
+
     @PostMapping("/userorders")
     public String userOrders(@RequestParam String email) {
         System.out.println("email issj........" + email);
-        String ans = new RDBMS_TO_JSON().generateJSON("SELECT * FROM foodfusion.orders "
-                + "INNER JOIN foodfusion.cart ON orders.cart_id = cart.cart_id && cart.useremail='" + email + "'"
+        String ans = new RDBMS_TO_JSON().generateJSON("SELECT * FROM orders "
+                + "INNER JOIN cart ON orders.cart_id = cart.cart_id && cart.useremail='" + email + "'"
                 + " ORDER BY orders.created_at DESC ");
-        
+
         return ans;
     }
-    
+
 }
